9c111cc0e088233a1805a3abe9fc49ae1a4f652e
Kevin Forero <kforero@dondoctor.com>
2021-10-18 22:12:11 -0500

    Ajustes

--- API_GestionTareas/Controllers/TareasController.cs
+++ API_GestionTareas/Controllers/TareasController.cs
@@ -14,44 +14,9 @@ namespace API_GestionTareas.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [AllowAnonymous]
+    [Authorize]
     public class TareasController : ControllerBase
     {
-        /// <summary>
-        /// Get a list with all the user tasks
-        /// </summary>
-        /// <param name="IdUsuario">user id</param>
-        /// <returns>A tasks list</returns>
-        //[Route("~/api/ConsTareasEmpleado")]
-        //[HttpGet]
-        //public RespuestaGestion ConsTareasEmpleado(int IdUsuario)
-        //{
-        //    RespuestaGestion Respuesta = new RespuestaGestion();
-        //   // var context = new GestionContext();
-        //    List<SP_ConsultarTareas_Result2> Tareas;
-        //    try
-        //    {
-        //        object[] parameters = new[] { IdUsuario.ToString() };
-        //        Tareas = context.Consultar_Tareas.FromSql("[dbo].[SP_ConsultarTareas] @p0", parameters: parameters).ToList();
-
-        //        if (Tareas.Count > 0)
-        //        {
-        //            Respuesta.Valido = true;
-        //            Respuesta.Tareas = Tareas;
-        //        }
-        //        else {
-        //            Respuesta.Valido = true;
-        //            Respuesta.Descripcion = "Este Empleado no tiene tareas";
-        //        }
-        //    }
-        //    catch (Exception ex) {
-
-        //        Respuesta.Valido = false;
-        //        Respuesta.Error = ex.Message;
-        //    }
-        //    return Respuesta;
-        //}
-
         [HttpGet]
         public ActionResult<RespuestaGestion> Get([FromQuery] TareasFilter filter)
         {
